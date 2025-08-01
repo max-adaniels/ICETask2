a34861039ecc12775eb5f030c830c8661fe57cd6
max-adaniels <160715841+max-adaniels@users.noreply.github.com>
2025-08-01 13:47:33 +0200

    Add project files.

---
+++ TestCarsIndexer/Test1.cs
@@ -0,0 +1,45 @@
+﻿using prjICETask2;
+
+namespace TestCarsIndexer
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestCarBrand()
+        {
+            Cars car = new Cars("Toyota", "Corolla", "Red", 2020, 20000.0);
+            Assert.AreEqual("Toyota", car[0]);
+        }
+
+        [TestMethod]
+        public void TestCarModel()
+        {
+            Cars car = new Cars("Toyota", "Corolla", "Red", 2020, 20000.0);
+            Assert.AreEqual("Corolla", car["carModel"]);
+        }
+
+        [TestMethod]
+        public void TestCarColor()
+        {
+            Cars car = new Cars("Toyota", "Corolla", "Red", 2020, 20000.0);
+            Assert.AreEqual("Red", car[2]);
+        }
+
+        [TestMethod]
+        public void TestUpdateCarYear()
+        {
+            Cars car = new Cars("Toyota", "Corolla", "Red", 2020, 20000.0);
+            car[3] = 2021;
+            Assert.AreEqual(2021, car[3]);
+        }
+
+        [TestMethod]
+        public void TestUpdateCarPrice()
+        {
+            Cars car = new Cars("Toyota", "Corolla", "Red", 2020, 20000.0);
+            car[4] = 21000.0;
+            Assert.AreEqual(21000.0, car[4]);
+        }
+    }
+}
---
+++ prjICETask2/Cars.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjICETask2
+{
+    public class Cars
+    {
+        private string carBrand, carModel, carColor;
+        private int carYear;
+        private double carPrice;
+
+        public Cars(string carBrand, string carModel, string carColor, int carYear, double carPrice)
+        {
+            this.carBrand = carBrand;
+            this.carModel = carModel;
+            this.carColor = carColor;
+            this.carYear = carYear;
+            this.carPrice = carPrice;
+        }
+
+        public Object this[int index]
+        {
+            get
+            {
+                return index switch
+                {
+                    0 => carBrand,
+                    1 => carModel,
+                    2 => carColor,
+                    3 => carYear,
+                    4 => carPrice,
+                    _ => null
+                };
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        carBrand = (string)value;
+                        break;
+                    case 1:
+                        carModel = (string)value;
+                        break;
+                    case 2:
+                        carColor = (string)value;
+                        break;
+                    case 3:
+                        carYear = (int)value;
+                        break;
+                    case 4:
+                        carPrice = (double)value;
+                        break;
+                }
+            }
+        }
+
+        public Object this[string attrName]
+        {
+            get
+            {
+                return attrName switch
+                {
+                    "carBrand" => carBrand,
+                    "carModel" => carModel,
+                    "carColor" => carColor,
+                    "carYear" => carYear,
+                    "carPrice" => carPrice,
+                    _ => null
+                };
+            }
+            set
+            {
+                switch (attrName)
+                {
+                    case "carBrand":
+                        carBrand = (string)value;
+                        break;
+                    case "carModel":
+                        carModel = (string)value;
+                        break;
+                    case "carColor":
+                        carColor = (string)value;
+                        break;
+                    case "carYear":
+                        carYear = (int)value;
+                        break;
+                    case "carPrice":
+                        carPrice = (double)value;
+                        break;
+                }
+            }
+        }
+    }
+}
