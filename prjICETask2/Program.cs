c23485f0600627662d570c751b6d44b9b0f64100
KyleLotter <ST10336577@vcconnect.edu.za>
2025-08-04 13:24:19 +0200

    Implemented the code that manages multiple index objects.

--- prjICETask2/Program.cs
+++ prjICETask2/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Cars car = new Cars("Toyota", "Corolla", "Red", 2020, 20000.0);
+            Cars car2 = new Cars("Honda", "Civic", "Blue", 2019, 22000.0);
+
+            Console.WriteLine("Car brand: " + car[0] + "\nCar Model: " + car[1] + "\nCar Colour: " + car[2] + "\nCar Year: " + car[3] + "\nCar Price: " + car[4]);
+
+            Console.WriteLine("\nCar brand: " + car2["carBrand"] + "\nCar Model: " + car2["carModel"] + "\nCar Colour: " + car2["carColor"] + "\nCar Year: " + car2["carYear"] + "\nCar Price: " + car2["carPrice"]);
+
         }
     }
 }
